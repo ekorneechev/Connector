aaf2e529e7c7098121bfed9601d2175c78ece22e
Korneechev Evgeniy <ekorneechev@gmail.com>
2018-07-11 12:35:11 +0300

    Tray indicator: several changes - Added button for show main window - Fixed hide of main window

--- source/gui.py
+++ source/gui.py
@@ -49,32 +49,18 @@ def initSignal(gui):
         GLib.idle_add(install_glib_handler, sig, priority=GLib.PRIORITY_HIGH)
 
 class TrayIcon:
-    """Класс, описывающий индикатор и меню в трее
+    """Класс, описывающий индикатор и меню в трее (пока только для MATE)
        Thanks: https://eax.me/python-gtk/"""
     def __init__(self, icon, menu):
         self.menu = menu
+        self.ind = Gtk.StatusIcon()
+        self.ind.set_from_file(icon)
+        self.ind.connect('popup-menu', self.onTrayMenu)
+        self.ind.set_tooltip_text("Программа Connector")
 
-        APPIND_SUPPORT = 1
-        try:
-            from gi.repository import AppIndicator3
-        except:
-            APPIND_SUPPORT = 0
-
-        if APPIND_SUPPORT == 1:
-            self.ind = AppIndicator3.Indicator.new(
-                appid, icon,
-                AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-            self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-            self.ind.set_menu(self.menu)
-        else:
-            self.ind = Gtk.StatusIcon()
-            self.ind.set_from_file(icon)
-            self.ind.connect('popup-menu', self.onPopupMenu)
-
-    def onPopupMenu(self, icon, button, time):
+    def onTrayMenu(self, icon, button, time):
         self.menu.popup(None, None, Gtk.StatusIcon.position_menu, icon,
                         button, time)
-
 class Gui:
     def __init__(self):
         self.prefClick = False
@@ -1185,6 +1171,14 @@ class Gui:
 
     def onShutdown(self, *args):
         os.system("mate-session-save --shutdown-dialog")
+
+    def onShowWindow(self, *args):
+        self.window.show_all()
+
+    def onHideWindow(self, *args):
+        self.window.hide()
+        return True
+
 
 def f_main():
     try:
