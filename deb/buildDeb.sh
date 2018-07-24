898c86cfe6f26d1defaab107bb2b945f6ff96d2d
Korneechev Evgeniy <ekorneechev@gmail.com>
2018-07-24 15:29:58 +0300

    buildDeb.sh: updated

--- deb/buildDeb.sh
+++ deb/buildDeb.sh
@@ -1,25 +1,34 @@
 #! /bin/bash
-rm -rf connector*
-mkdir -p connector/usr/bin
-mkdir -p connector/usr/share/applications/data
-mkdir -p connector/usr/share/connector
-mkdir -p connector/usr/share/man/man1
-mkdir -p connector/etc/connector
-cp ../source/*.py connector/usr/share/connector/
-cp ../source/connector connector/usr/bin/
-cp -r ../data/ connector/usr/share/connector/
-mv connector/usr/share/connector/data/connector.desktop connector/usr/share/applications/data
-mv connector/usr/share/connector/data/connector.man connector/usr/share/man/man1/connector.1
-mv connector/usr/share/connector/data/kiosk.access connector/etc/connector
-chmod 755 connector/usr/bin/connector
+TARGET=connector
+BIN=$TARGET/usr/bin
+SHARE=$TARGET/usr/share
+PREFIX=$SHARE/$TARGET
+DATA=$PREFIX/data
+APPS=$SHARE/applications
+MAN=$SHARE/man/man1
+ETC=$TARGET/etc/$TARGET
+MIME=$SHARE/mime/packages
+ICONS=$SHARE/icons/hicolor/64x64/apps
+
+rm -rf $TARGET*
+mkdir -p $BIN $APPS $PREFIX $MAN $ETC $MIME $ICONS
+cp ../source/*.py $PREFIX/
+cp ../source/$TARGET $BIN/
+cp -r ../data/ $PREFIX/
+mv $DATA/$TARGET.desktop $APPS
+mv $DATA/$TARGET.man $MAN/$TARGET.1
+mv $DATA/kiosk.access $ETC
+mv $DATA/emblem $ICONS/$TARGET.png
+mv $DATA/$TARGET.xml $MIME
+chmod 755 $BIN/$TARGET
 INST_SIZE=`du -s connector | cut -f 1`
-mkdir -p connector/DEBIAN
-cd connector
+mkdir -p $TARGET/DEBIAN
+cd $TARGET
 md5deep -rl usr > DEBIAN/md5sums
 md5deep -rl etc >> DEBIAN/md5sums
 cd ..
-cp control conffiles connector/DEBIAN/
-sed -i "s\Installed-Size:\Installed-Size: $INST_SIZE\g" connector/DEBIAN/control
-fakeroot dpkg-deb --build connector
-mv connector.deb connector_`grep Version control | sed s/Version:\ //g`_all.deb
-rm -r connector/
+cp control conffiles $TARGET/DEBIAN/
+sed -i "s\Installed-Size:\Installed-Size: $INST_SIZE\g" $TARGET/DEBIAN/control
+fakeroot dpkg-deb --build $TARGET
+mv $TARGET.deb ${TARGET}_`grep Version control | sed s/Version:\ //g`_all.deb
+rm -r $TARGET/
