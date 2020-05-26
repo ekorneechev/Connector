d68dde7cc530143b98344177f23b9e7e5ae881ed
Korneechev Evgeniy <ekorneechev@gmail.com>
2020-05-26 15:02:55 +0300

    kiosk.py: fixed urlfix

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -64,7 +64,8 @@ def enable_kiosk( mode = "kiosk" ):
 def fix_shortcut(mode, _input, output):
     """Replace variable in the desktop files"""
     shortcut = "%s/connector-%s.desktop" % (_etc_dir, mode)
-    os.system ("sed -i \"s|\%s|'%s'|g\" %s" % (_input, output, shortcut))
+    if output: output = "'%s'" % output
+    os.system ("sed -i \"s|\%s|%s|g\" %s" % (_input, output, shortcut))
 
 def enable_kiosk_ctor(_file):
     """Exec connector (with ctor-file) in the mode KIOSK"""
