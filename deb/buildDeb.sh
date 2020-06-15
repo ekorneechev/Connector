8a4a840c35dc84d499001eee406f9924c26997e4
Korneechev Evgeniy <ekorneechev@gmail.com>
2020-06-15 17:40:55 +0300

    Added license and (c) to files

--- deb/buildDeb.sh
+++ deb/buildDeb.sh
@@ -1,4 +1,20 @@
-#! /bin/bash
+#!/bin/bash
+
+# MyConnector
+# Copyright (C) 2020 Evgeniy Korneechev <ek@myconnector.ru>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the version 2 of the GNU General
+# Public License as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
 TARGET=myconnector
 USR=$TARGET/usr
 BIN=$USR/bin
--- lib/connector.py
+++ lib/connector.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# MyConnector
+# Copyright (C) 2020 Evgeniy Korneechev <ek@myconnector.ru>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the version 2 of the GNU General
+# Public License as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
 import myconnector.options as options
 from myconnector.params import *
 from re import escape
