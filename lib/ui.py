251cd6476ba46c0a71fc0e41eb42b999e2ad8f41
Korneechev Evgeniy <ekorneechev@gmail.com>
2020-06-16 11:06:30 +0300

    Updated AboutDialog

--- lib/ui.py
+++ lib/ui.py
@@ -294,16 +294,29 @@ class Gui(Gtk.Application):
 
     def onViewAbout(self, *args):
         """Создает диалоговое окно 'О программе'"""
-        about = Gtk.AboutDialog(parent = self.window)
+        about = Gtk.AboutDialog( parent = self.window )
         about.set_program_name( "MyConnector (ex. Connector)" )
-        comments = """Программа-фронтэнд для удаленного администрирования
-                      компьютеров с различными операционными системами.
-                      Поддерживается большинство распространенных типов подключения.""".replace('  ','')
-        about.set_comments(comments)
-        about.set_version(VERSION + " (release: " + RELEASE + ')')
-        about.set_website("http://myconnector.ru")
-        about.set_website_label("Сайт проекта")
-        about.set_copyright("© Корнеечев Е.А., 2014-2020\ne-mail:ek@myconnector.ru\n\nPayPal: ekorneechev@gmail.com\nWMR: R305760666573, WMZ: Z841082507423")
+        about.set_comments(
+            "Программа-фронтэнд для удаленного администрирования\n"
+            "компьютеров с различными операционными системами.\n"
+            "Поддерживается большинство распространенных типов подключения." )
+        about.set_version( "%s (release: %s)" % ( VERSION, RELEASE ) )
+        about.set_website( "http://myconnector.ru" )
+        about.set_website_label( "http://myconnector.ru" )
+        about.set_license(
+            "MyConnector\n\n"
+            "This program is free software; you can redistribute it and/or\n"
+            "modify it under the terms of the version 2 of the GNU General\n"
+            "Public License as published by the Free Software Foundation.\n\n"
+            "This program is distributed in the hope that it will be useful,\n"
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+            "GNU General Public License for more details.\n\n"""
+            "You should have received a copy of the GNU General Public License\n"
+            "along with this program. If not, see http://www.gnu.org/licenses/." )
+        about.set_copyright(
+            "© Korneechev E.A., 2014-2020\n"
+            "ek@myconnector.ru" )
         about.set_logo_icon_name( "myconnector" )
         about.run()
         about.destroy()
